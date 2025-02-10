5dedb82bf108af84b2cb363d340e9bfc70fa0231
Parth Agrawal <pvagrawal0@gmail.com>
2025-02-10 00:13:29 -0500

    create post endpoint to hit

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,7 @@
+import ai from "./services";
 import { ChatContext } from "./types";
 
 export const postChat = async ({ body }: ChatContext) => {
-    console.log(body.message)
-    return "Hello Post Chat"
+    const response = await ai.respondToChat(body.message, body.phone);
+    return response;
 }
