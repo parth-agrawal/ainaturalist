dd232d9eca8e086a5927fa50c9b002bf65b91836
Parth Agrawal <pvagrawal0@gmail.com>
2025-02-10 00:53:32 -0500

    add twilio webhook

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,24 @@
+import { Context } from "elysia";
 import ai from "./services";
 import { ChatContext } from "./types";
 
 export const postChat = async ({ body }: ChatContext) => {
     const response = await ai.respondToChat(body.message, body.phone);
     return response;
+}
+
+interface TwilioRequestBody {
+    Body: string;
+    From: string;
+}
+
+export const twilioWebhook = async (c: Context & { body: TwilioRequestBody }) => {
+    const { Body: message, From: phone } = c.body;
+    const response = await ai.respondToChat(message, phone);
+
+
+    return new Response(
+        `<?xml version="1.0" encoding="UTF-8"?><Response><Message>${response}</Message></Response>`,
+        { headers: { 'Content-Type': 'application/xml' } }
+    )
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Elysia } from "elysia";
-import { postChat } from "./controller";
+import { postChat, twilioWebhook } from "./controller";
 import { ChatRequest } from "./types";
 
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .post('/chat', postChat, ChatRequest)
+  .post('/twilio/webhook', twilioWebhook)
   .listen(3000);
 
 console.log(
